5389430211bf37cac9c5c8b5cc3a192b194942e4
Sean Xiaowen Zhang <sean@evestemptation.com>
2017-04-25 17:20:46 -0700

    add static server

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 "use strict";
 
 const express = require("express");
+const bluebird = require("bluebird");
 const {winston} = require("./globals.js");
 const exphbs  = require('express-handlebars');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
 var app = express();
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+require('dotenv').config();
 
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
@@ -12,8 +21,11 @@ app.set('view engine', 'handlebars');
 
 app.use(morgan('combined'))
 
+app.use(express.static('public'));
 app.get('/', function (req, res) {
     res.render('home');
 });
 
 app.listen(3000);
+console.log("Express started at port:3000");
+module.exports = {app};

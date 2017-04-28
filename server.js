00380b2a6c0ca2721bfd20a980f8a9569268841a
Xiaowen Zhang <seanxwzhang@gmail.com>
2017-04-27 18:02:16 -0700

    added ssl

--- server.js
+++ server.js
@@ -3,6 +3,9 @@
 // dot env for environment variables
 require('dotenv').config();
 
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
 const express = require("express");
 const bluebird = require("bluebird");
 const exphbs  = require('express-handlebars');
@@ -10,6 +13,17 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const {winston, redisClient} = require("./globals.js");
 const favicon = require('serve-favicon');
+
+if (process.env.NODE_ENV == 'prod') {
+    var privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
+    var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
+} else {
+    var privateKey  = fs.readFileSync('sslcert/localhost.key', 'utf8');
+    var certificate = fs.readFileSync('sslcert/localhost.crt', 'utf8');
+}
+
+var credentials = {key: privateKey, cert: certificate};
+
 
 var app = express();
 // serve favicon
@@ -39,6 +53,11 @@ app.use((err, req, res, next) => {
     }
 })
 
-app.listen(process.env.PORT);
-console.log(`Express started at port:${process.env.PORT}`);
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(process.env.UNSECURE_PORT);
+httpsServer.listen(process.env.SECURE_PORT);
+console.log(`Express started at port:${process.env.UNSECURE_PORT}`);
+console.log(`Express started at port:${process.env.SECURE_PORT}`);
 module.exports = {app};

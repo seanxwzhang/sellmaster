12cd930529ec8430d80203bd63be1a5ce55b2854
Sean Xiaowen Zhang <sean@evestemptation.com>
2017-05-06 13:53:36 -0700

    wrote shopify http client

--- controller/client.js
+++ controller/client.js
@@ -83,7 +83,56 @@ class eBayClient {
  * Shopify http client
  **/
 class ShopifyClient {
+    constructor(storename) {
+        this.storename = storename;
+        this.baseUrl = `https://${storename}.myshopify.com`;
+        this.headers = {
+            'X-Shopify-Access-Token': ''
+        };
+        this.authKey = undefined;
 
+
+    }
+
+    _request(method) {
+        let client = this;
+        return(url, qs, data) => {
+            let uri = [client.baseUrl, url].join('/');
+            console.log(uri);
+            return new Promise((res, rej) => {
+                if (typeof client.authKey !== "undefined") {
+                    res(client.headers);
+                } else {
+                    return redisClient.getAsync(getTockenKey("shopify", client.storename))
+                    .then((token) => {
+                        client.authKey = token.replace(/\"/g, '');
+                        client.headers['X-Shopify-Access-Token'] = client.authKey;
+                        res(client.headers);
+                    }).catch((err) => {
+                        rej("Error occured in acquiring ebay access token, please have the store login first: " + err);
+                    })
+                }
+            }).then((headers) => {
+                return rp({
+                    method: method,
+                    uri: uri,
+                    qs: qs,
+                    headers: headers,
+                    body: JSON.stringify(data),
+                    resolveWithFullResponse: true,
+                    simple: false
+                }).then((response) => {
+                    if (response.statusCode >= 200 && response.statusCode < 300) {
+                        return response.body;
+                    } else {
+                        return response.body;
+                    }
+                }).catch((err) => {
+                    throw err;
+                })
+            })
+        }
+    }
 }
 
 module.exports = {eBayClient, ShopifyClient};
